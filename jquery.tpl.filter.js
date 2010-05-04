4f8bc01e67b53d1da46ac27cc6a5bfe153ce0273
Fedor Indutny <fedor.indutny@gmail.com>
2010-05-04 08:31:08 +0700

    Commented out filter module

--- jquery.tpl.filter.js
+++ jquery.tpl.filter.js
@@ -1,5 +1,8 @@
 (function($ , undefined) {
 	var
+		/**
+		* Expose all filters
+		*/
 		filters = $.template.filters = {
 			"escape": function ( str ) {
 				return escape(str);
@@ -14,6 +17,9 @@
 				return str.replace(/<|>|&|"|'/g, safe);
 			}
 		},
+		/**
+		* Replacements for safe filter
+		*/
 		safeArr = {
 			"<" : "&lt;",
 			">" : "&gt;",
@@ -21,25 +27,49 @@
 			"\"" : "&quot;",
 			"'" : "&#039;"
 		};
-		
+	/**
+	* This filter will be used if you call unexisting filtername
+	* @param{string} str Input string
+	* @return{string}
+	*/
 	function nopfilter ( str ) {return str};
 	
+	/**
+	* "Safe" replacement function
+	* @param{string} str Input string
+	* @return{string}
+	*/
 	function safe( str ) {
 		return safeArr[str] || str;
 	}
 	
+	/**
+	* This function will be exposed by plugin into template as $scope.$filter
+	* @param{string} filter Filter name
+	* @param{function} code Scope returning array of strings
+	*/
 	function filter_func(filter , code) {		
 		return (filters[filter] || nopfilter)(code([]).join(""));
 	}
 	
+	/**
+	* Add new modificators
+	*/
 	$.extend($.template.modificators,{
 		"filter": function ( filter , namespace) {
-		
+			
+			/**
+			* Initiate namespace (add $filter function)
+			*/
 			!namespace.$filter
 				&&
 					(namespace.$filter = filter_func);
 		
+			/**
+			* Trim spaces from name or use "escape" if no filter name was passed
+			*/
 			filter = $.trim(filter) || "escape";
+			
 			return "$p($scope.$filter('" + filter + "',function($_){";
 		},
 		"/filter" : function ( namespace) {
